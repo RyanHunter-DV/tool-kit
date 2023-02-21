d74486fa04a313dc537a13b84aea040f16442f53
ryanhunter <ryanhunter@rhTestPC.dynamic>
2023-02-21 07:19:03 -0500

    add decompress feature

--- packer/lib_v1/mainentry.rb
+++ packer/lib_v1/mainentry.rb
@@ -8,13 +8,23 @@ class MainEntry
 	attr :allfiles;
 	attr :pak; # the packed file
 
-	def initialize r
-		@root = File.absolute_path(r);
+	attr :mode; # :compress,:decompress
+	attr :source;
+	def optionProcess ##{{{
+		@mode = :compress; # default
+		@mode = :decompress if ARGV[0]=='-d';
+		@root = File.absolute_path(ARGV[0]) if @mode==:compress;
+		@source = File.absolute_path(ARGV[1]) if @mode==:decompress;
+	end ##}}}
+	def initialize
+		optionProcess;
 		@exitSig = 0;
 		@debug = false;
-		@allfiles= [];
-		@pak = '__installerPacker__.pak';
-		@pakh= File.open(@pak,'w');
+		if @mode==:compress
+			@allfiles= [];
+			@pak = '__installerPacker__.pak';
+			@pakh= File.open(@pak,'w');
+		end
 	end
 
 	def debug(msg)
@@ -40,7 +50,9 @@ class MainEntry
 	# change: /home/user/dir/file
 	# to: <PACKER_ROOT>/file
 	def __filehead__(f) ##{{{
-		return f.sub("#{@root}/",'# <PACKER> - ');
+		filterout = File.dirname(@root);
+		line = f.sub("#{filterout}/",'# <PACKER> - ');
+		line += ",#{File.stat(f).mode}";
 	end ##}}}
 
 	def __packfile__(f) ##{{{
@@ -61,14 +73,54 @@ class MainEntry
 
 	def run
 		debug("root: #{@root}");
-		# find all files
-		subs = Dir.children(@root);
-		__getsubs__(@root);
-		debug("#{@allfiles}");
+		if @mode==:decompress
+			decompress;
+		else
+			# find all files
+			subs = Dir.children(@root);
+			__getsubs__(@root);
+			debug("#{@allfiles}");
 
-		__packall__;
-		@pakh.close();
+			__packall__;
+			@pakh.close();
+		end
 		return @exitSig;
 	end
+
+	def decompress ##{{{
+		fh = File.open(@source,'r');
+		cnts = fh.readlines();
+		cfile='';
+		cnts.each do |line|
+			line.chomp!;
+			ptrn = Regexp.new('\<PACKER\> - (.+),(\d+)');
+			md = ptrn.match(line);
+			if md
+				fh.close if cfile!='';
+				cfile = md[1];
+				fmode = md[2].to_i;
+				createRecursiveDir(cfile,fmode);
+				fh = File.open(cfile,'w');
+				puts "file: #{cfile} decompressed" if cfile!='';
+			end
+			fh.write(line+"\n");
+		end
+	end ##}}}
+
+	def createRecursiveDir(f,mode) ##{{{
+		stack = [];
+		created = false;
+		d = File.dirname(f);
+		while (not Dir.exists?(d)) do
+			stack << d;
+			d = File.dirname(d);
+		end
+		stack.reverse!;
+		stack.each do |d|
+			Dir.mkdir(d);
+		end
+		# create file
+		fh = File.new(f,'w',mode);fh.close;
+	end ##}}}
 
 end
--- packer/test/be/boot/bin/bootShell.bash
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env bash
-cdir=/home/ryanhunter/Projects/GitHub/rhFlow/be/v2/test/be/boot/bin
-version=v1
-export SHELLTYPE=bash
-source ${cdir}/bootenv-${version}.bash
