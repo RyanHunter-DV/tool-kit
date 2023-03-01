ff0e1a28160aa5516409c0281324c7ad3c2c547d
ryanhunter <ryanhunter@rhTestPC.dynamic>
2023-03-01 07:16:56 -0500

    up

--- mdc/ruby/v2/lib_v1/codelib.rb
+++ mdc/ruby/v2/lib_v1/codelib.rb
@@ -16,4 +16,35 @@ class Codelib
 		fh = File.open(tmpfile,'w');fh.close();
 		cmd = %Q|/local_vol1_nobackup/ryanh/Git/codelib/codelib-main/bin/cb-rb -i #{@codeid} -f #{tmpfile},1|;
 		out,err,st = Open3.capture3(cmd);
-		raise RunException.new("codelib process e
+		raise RunException.new("codelib process error(codeid: #{@codeid})",5) if st.exitstatus!=0;
+		fh = File.open(tmpfile,'r');
+		@codes = fh.readlines(); fh.close(); File.delete(tmpfile);
+		@codes.map!{|l| l.chomp;};
+		return;
+	end
+
+	def apio(api,ovrd)
+		ovrd.map!{|l| "\t"+l;};
+		ovrd.insert(0,"def #{api} ## {{{");
+		ovrd << "end ## }}}";
+		ptrn = Regexp.new("def +#{api}");
+		ncodes=[];
+		sindex=0;
+		filter=false;
+		@codes.each_with_index do |l,i|
+			if ptrn.match(l);
+				filter=true;
+				sindex=i;
+			end
+			ncodes<<l if not filter;
+			if filter
+				filter=false if l=="\tend";
+			end
+		end
+		ovrd.map!{|l| "\t"+l;};
+		# ovrd.reverse!;
+		ncodes.insert(sindex,*ovrd);
+		@codes = ncodes;
+	end
+
+end
